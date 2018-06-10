89072116e39fa270766b7009798206092ca15c6f
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2018-06-10 21:10:50 +0200

    Moved some stuff, added hero with image

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,13 @@ Encore
   .enableSassLoader()
   .cleanupOutputBeforeBuild()
   .enableBuildNotifications()
-
+  .configureFilenames({
+    images: 'images/[name].[ext]'
+  })
   // Add entries.
   .addEntry('js/navigation', './assets/js/navigation.js')
   .addEntry('js/alert', './assets/js/alert.js')
+  .addEntry('js/app', './assets/js/app.js')
   .addStyleEntry('css/app', './assets/scss/app.scss')
 ;
 
