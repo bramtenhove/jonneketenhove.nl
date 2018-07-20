912963afa7585de2784d5b718f6e2a6a3bc02031
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2018-07-20 20:44:12 +0200

    Use Twig cache for increased performance

--- public/index.php
+++ public/index.php
@@ -3,7 +3,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../templates');
-$twig = new \Twig_Environment($loader);
+$twig = new Twig_Environment($loader, array(
+  'cache' => '../cache',
+));
 
 // Render the index page.
 echo $twig->render('index.twig');
