80d4dc7174886064ac8642251ef0e5d04870f713
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2018-07-20 21:47:22 +0200

    Add YouTube settings and escape event

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,30 @@ import ModalVideo from 'modal-video';
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  document.querySelectorAll('article.tile').forEach(function(element) {
-
+  // Setup modal videos for all video items.
+  new ModalVideo('.modal-video-item', {
+    youtube: {
+      autoplay: 1,
+      controls: 1,
+      modestbranding: true,
+      nocookie: true
+    }
   });
 
-  new ModalVideo('.modal-video-item');
+  // Make sure escape kills the modal window.
+  document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    let isEscape = false;
+    if ("key" in evt) {
+      isEscape = (evt.key == "Escape" || evt.key == "Esc");
+    }
+    else {
+      isEscape = (evt.keyCode == 27);
+    }
+
+    if (isEscape && document.getElementsByClassName('js-modal-video-dismiss-btn').length > 0) {
+      document.querySelector('.js-modal-video-dismiss-btn').click();
+    }
+  };
 
 });
