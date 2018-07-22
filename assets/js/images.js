2de022a2a5559fb1652867d77988feb540a6a93c
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2018-07-22 14:30:47 +0200

    Add more content and add dark background over images

--- assets/js/images.js
+++ assets/js/images.js
@@ -6,6 +6,7 @@ require('../images/chat.jpg');
 require('../images/timescapes.jpg');
 require('../images/timescapes2.jpg');
 require('../images/proefeet.jpg');
+require('../images/proefeet2.jpg');
 require('../images/kerk.jpg');
 require('../images/kerk2.jpg');
 require('../images/kerk3.jpg');
