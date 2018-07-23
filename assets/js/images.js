a7fd1b18abaf274e8dcae4b7095f87337e458809
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2018-07-23 20:08:03 +0200

    Change images and add some text

--- assets/js/images.js
+++ assets/js/images.js
@@ -1,12 +1,11 @@
 require('../images/header.jpg');
 require('../images/ik.jpg');
 
-require('../images/kookboek.jpg');
+require('../images/bakkie.jpg');
 require('../images/chat.jpg');
-require('../images/timescapes.jpg');
-require('../images/timescapes2.jpg');
-require('../images/proefeet.jpg');
-require('../images/proefeet2.jpg');
 require('../images/kerk.jpg');
 require('../images/kerk2.jpg');
-require('../images/kerk3.jpg');
+require('../images/kookboek.jpg');
+require('../images/proefeet.jpg');
+require('../images/timescapes.jpg');
+require('../images/timescapes2.jpg');
